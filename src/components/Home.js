bfb7c436c507be24855ab5da2cf133f14f847691
Your Name <bundalageofrey2@gmail.com>
2019-11-30 13:19:16 +0300

    folder arrangement change welcome page contents

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div  className="container">
+               <div className="row">
+                   <div className="">
+                           <h4>Home.</h4>
+                   </div>
+               </div>
+            </div>
+        )
+    }
+}
---
+++ src/components/Welcome.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Welcome extends Component {
+    render() {
+        return (
+                <div className='qn_home'>
+                 <div  className="container">
+               <div className="row">
+                   <div className="col-md-6 offset-md-3 pt-4 ">
+                           <h4 className="qn_banner_text">Together We can be where we want to be.</h4>
+                   </div>
+               </div>
+            </div>
+            </div>
+           
+        )
+    }
+}
